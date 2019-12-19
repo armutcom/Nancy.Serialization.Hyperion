b31232ff6f012a261d30df31c6f97e10a33d8198
deniz@armut.com <deniz@armut.com>
2019-12-19 18:43:31 +0300

    mono tests

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 #exit if any command fails
 set -e
 
-export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.6.1/
+export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.6.1-api/
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp3.0
+dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp3.1
 
 # https://github.com/xunit/xunit/issues/1573
 dotnet build ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f net461
