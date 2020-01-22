42582c58bbc0e58e665a5951de61556b18d9875c
deniz@armut.com <deniz@armut.com>
2020-01-22 14:30:57 +0300

    fix test runner for mono

--- build.sh
+++ build.sh
@@ -10,9 +10,11 @@ nuget install xunit.runner.console -Version 2.4.1 -OutputDirectory ./testrunner
 
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+echo Running netcoreapp3.1 tests
 dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp3.1 --no-build
+echo Running netcoreapp2.1 tests
 dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp2.1 --no-build
-
+echo Running net461 tests
 mono ./testrunner/xunit.runner.console.2.4.1/tools/net461/xunit.console.exe ./tests/Nancy.Serialization.Hyperion.Tests/bin/Release/net461/Nancy.Serialization.Hyperion.Tests.dll
 
 # # https://github.com/xunit/xunit/issues/1573
