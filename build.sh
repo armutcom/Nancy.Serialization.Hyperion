4595fe56cc3e43d091ea4cbbed4d9d275a7898e3
deniz@armut.com <deniz@armut.com>
2020-01-22 14:23:41 +0300

    fix test runner for mono

--- build.sh
+++ build.sh
@@ -5,10 +5,16 @@ set -e
 
 export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.6.1-api/
 
+mkdir testrunner
+nuget install xunit.runner.console.2.4.1 -OutputDirectory ./testrunner
+
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp3.1
+dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp3.1 --no-build
+dotnet test ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f netcoreapp2.1 --no-build
 
-# https://github.com/xunit/xunit/issues/1573
-dotnet build ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f net461
-(cd ./tests/Nancy.Serialization.Hyperion.Tests && exec dotnet xunit -f net461)
+mono ./testrunner/xunit.runner.console.2.4.1/tools/net461/xunit.console.exe ./tests/Nancy.Serialization.Hyperion.Tests/bin/Release/net461/Nancy.Serialization.Hyperion.Tests.dll
+
+# # https://github.com/xunit/xunit/issues/1573
+# dotnet build ./tests/Nancy.Serialization.Hyperion.Tests -c Release -f net461
+# (cd ./tests/Nancy.Serialization.Hyperion.Tests && exec dotnet xunit -f net461)
